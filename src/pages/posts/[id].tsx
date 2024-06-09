8db7108a14761ccaf14e33a838b18fe315f1c3c1
frozar <fabien.rozar@gmail.com>
2024-06-09 17:34:30 +0400

    Use the instruction "use server" to get data with type: delete api directory

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -1,17 +1,16 @@
 import { RouteSectionProps, createAsync } from "@solidjs/router";
 import { Show } from "solid-js";
+import { getPost } from "~/lib/posts";
 
 export default function PostView(props: RouteSectionProps) {
-  const post = createAsync(async () =>
-    (await fetch(`/api/posts/${props.params.id}`)).json()
-  );
+  const post = createAsync(async () => getPost(+props.params.id));
   return (
     <Show when={post()}>
       <article>
-        <h3>{post().title}</h3>
-        <small>{new Date(post().timestamp).toLocaleDateString()}</small>
-        <small>{new Date(post().timestamp).toLocaleDateString()}</small>
-        <div>{post().content}</div>
+        <h3>{post()!.title}</h3>
+        <small>{post()!.timestamp.toLocaleDateString()}</small>
+        <small>{post()!.timestamp.toLocaleDateString()}</small>
+        <div>{post()!.content}</div>
       </article>
     </Show>
   );
